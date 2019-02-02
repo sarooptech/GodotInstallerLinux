ead53ce5c2d5858a769d82c44e9d8ca7fd4cfa9a
sarooptech <sarooptech@gmail.com>
2019-02-03 01:40:36 +0530

    Added Functions
    Modified full script and devided it into various functions.
    Also Now You can download v2.1.5.
    Code is now more readable.

--- godotinstaller.sh
+++ godotinstaller.sh
@@ -3,175 +3,161 @@
 
 # Variable Declarations
 
+INSTALL_DIR="$HOME/Godot/" # To ceate version directory and install perticular version
+
+VERSION="" # For sttoring selected version
+
 MACHINE_TYPE=`uname -m`
 
-INSTALL_DIR="$HOME/Godot/"
+BIT=""
 
 DESKTOP_ENTRY="[Desktop Entry]
 Type=Application
 Name=Godot VERSION
 Icon=$HOME/Godot/icon.png
-Exec="$HOME/Godot/VERSION/Godot_vVERSION-stable_x11.BIT"
+Exec="$HOME/Godot/Godot_vVERSION-stable_x11.BIT"
 Comment=Game Engine
 Categories=Development;IDE;
 Terminal=false
 StartupWMClass=Godot"
-
-VERSION=""
-BIT=""
 #-----------------------------------------
+#Function Declerations
+function main(){
 
-echo -e "\e[36mWelcome to Godot Installer
-\e[32mClick 1: \e[39mTo Install Godot
-\e[32mClick 2: \e[39mTo Uninstall Godot
-\e[32mClick 0: \e[39mTo Exit"
-read -p "Enter selection [1,2,0] > "
+	checkbit
 
-if [[ $REPLY =~ ^[0-2]$ ]]; then
-	if [[ $REPLY == 0 ]]; then
-		echo "Exiting Godot Installer"
-		exit
+	echo -e "Welcome to Godot Installer
+	(1) To Install Godot
+	(2) To Uninstall Godot
+	(0) To Exit"
+
+	read -p "Enter selection [0,1,2] > "
+
+	if [[ $REPLY =~ ^[0-2]$ ]]; then
+		
+		if [[ $REPLY == 0 ]]; then
+			echo "Exiting Godot Installer"
+			exit
+		fi
+
+		if [[ $REPLY == 1 ]]; then
+			selectversion
+		fi
+
+		if [[ $REPLY == 2 ]]; then
+			echo "Here Goes Uninstall Code"
+		fi
+
+	# else
+	# 	echo "Invalid Entry" >&2
 	fi
+}
 
-	if [[ $REPLY == 1 ]]; then
-		echo "Select Godot Godot Version To Install"
-		echo "(1) Godot 3.0.6 - Stable (Prefered)"
-		echo "(2) Godot 2.1.5 - Old Stable"
-		echo "(3) Godot 3.1 - Latest Beta Release"
-		echo "(4) Install Export Templates"
-		echo "(0) Back to Main Menu"
-		read -p "Enter Selection[1,2,3,0] >"
-		if [[ $REPLY =~ ^[0-4]$ ]]; then
+#For Downloading Perticular Godot Version to Godot Directory
+function download(){
+	ceckbit
+	echo "Downloading Godot Using wget"
+	wget -P $INSTALL_DIR "https://downloads.tuxfamily.org/godotengine/$VERSION/Godot_v$VERSION-stable_x11.$BIT.zip"
+	
+	echo "Downloading Icon"
+	wget -P $INSTALL_DIR "https://upload.wikimedia.org/wikipedia/commons/thumb/6/6a/Godot_icon.svg/2000px-Godot_icon.svg.png"
+	cd "$INSTALL_DIR"
+	mv "2000px-Godot_icon.svg.png" "icon.png"
+	cd
+}
 
-			if [[ $REPLY == 0 ]]; then
-				echo "Back to Main Menu Command"
-			fi
+#For checking if if Godot Drectory exist or not
+function checkdir(){
+	echo "Checking if Downoad Directory Exist"
+	if [ ! -d "$INSTALL_DIR" ]; then
+		echo "Directory Does Not Exist. Creating One"
+		mkdir $HOME/Godot
+	else
+		echo "Directory Already Exist"
+	fi
+}
 
-			if [[ $REPLY == 1 ]]; then
+#For Making Desktop Entry
+function makedesktopentry(){
+	echo "Generating Desktp Entry"
+	DESKTOP_ENTRY=`echo "${DESKTOP_ENTRY//VERSION/$VERSION}"`
+	echo "${DESKTOP_ENTRY//BIT/$BIT}" >> "$HOME/Desktop/Godot_v$VERSION-stable_x11.$BIT.desktop"
+	#.local/share/applications/
+}
 
-				VERSION="3.0.6"
+#For Decompressing Downloaded Zip
+function decompress(){
+	unzip $HOME/Godot/*.zip -d $HOME/Godot/
+}
 
-				echo "Downloading and Installing Godot 3.0.6"
+#For deletind downloaded temp files
+function delete(){
+	echo "Deletind Zip Files From Installation Directory"
+	rm $HOME/Godot/*.zip
+}
 
-				if [ ! -d "$INSTALL_DIR" ]; then
+#Function For selecting Installation Version
+function selectversion(){
+	echo "Select Godot Version To Install
+	(1) Godot 3.0.6 - Stable (Prefered)
+	(2) Godot 2.1.5 - Old Stable
+	(3) Godot 3.1 - Latest Beta Release
+	(4) All Versions
+	(0) Back to Main Menu"
+	read -p "Enter Selection[1,2,3,0] >"
 
-					echo "Installation Directory Does not exitst."
-					echo "Creating Installation Directory in home directory of current user"
-					mkdir $HOME/Godot
-					mkdir $HOME/Godot/3.0.6
+	if [[ $REPLY =~ ^[0-3]$ ]]; then
 
-					if [ ${MACHINE_TYPE} == 'x86_64' ]; then
+		if [[ $REPLY == 0 ]]; then
+			main
+		fi
 
-						BIT="64"
+		if [[ $REPLY == 1 ]]; then
+			VERSION="3.0.6"
+			echo "Downloading $BIT bit Version of Godot $VERSION"
+			download
+			decompress
+			makedesktopentry
+			cleanup
+		fi
 
-						echo "Downloading 64bit Version of Godot"
-						wget -P $HOME/Godot/3.0.6 https://downloads.tuxfamily.org/godotengine/3.0.6/Godot_v3.0.6-stable_x11.64.zip
+		if [[ $REPLY == 2 ]]; then
+			VERSION="2.1.5"
+			echo "Downloading $BIT bit Version of Godot $VERSION"
+			download
+			decompress
+			makedesktopentry
+			cleanup
+		fi
+		if [[ $REPLY == 3 ]]; then
+			VERSION="3.1"
+			echo "Downloading $BIT bit Version of Godot $VERSION"
+			download
+			decompress
+			makedesktopentry
+			cleanup
+		fi
 
-						echo "Downloading Icon"
-						wget -P $HOME/Godot/ https://upload.wikimedia.org/wikipedia/commons/thumb/6/6a/Godot_icon.svg/2000px-Godot_icon.svg.png
-						mv $HOME/Godot/2000px-Godot_icon.svg.png $HOME/Godot/icon.png
-
-					else
-
-						BIT="32"
-
-						echo "Downloading 32bit Version of Godot"
-						wget -P $HOME/Godot/3.0.6 https://downloads.tuxfamily.org/godotengine/3.0.6/Godot_v3.0.6-stable_x11.32.zip
-
-						echo "Downloading Icon"
-						wget -P $HOME/Godot/ https://upload.wikimedia.org/wikipedia/commons/thumb/6/6a/Godot_icon.svg/2000px-Godot_icon.svg.png
-						mv $HOME/Godot/2000px-Godot_icon.svg.png $HOME/Godot/icon.png
-
-					fi
-
-					unzip $HOME/Godot/3.0.6/*.zip -d $HOME/Godot/3.0.6/
-
-                    echo "Deleting Downloaded Package"
-                    rm $HOME/Godot/3.0.6/*.zip
-
-                    echo "Generating Desktp Entry"
-                    DESKTOP_ENTRY=`echo "${DESKTOP_ENTRY//VERSION/$VERSION}"`
-                    echo "${DESKTOP_ENTRY//BIT/$BIT}" >> Godot_v$VERSION-stable_x11.$BIT.desktop
-                    #.local/share/applications/Godot_
-
-				else
-					echo "Installation Directory exitst."
-
-					if [ ! -d "$HOME/Godot/3.0.6/" ]; then
-						mkdir $HOME/Godot/3.0.6
-
-						if [ ${MACHINE_TYPE} == 'x86_64' ]; then
-
-							BIT="64"
-
-							echo "Downloading 64bit Version of Godot"
-							wget -P $HOME/Godot/3.0.6 https://downloads.tuxfamily.org/godotengine/3.0.6/Godot_v3.0.6-stable_x11.64.zip
-
-							echo "Downloading Icon"
-							wget -P $HOME/Godot/ https://upload.wikimedia.org/wikipedia/commons/thumb/6/6a/Godot_icon.svg/2000px-Godot_icon.svg.png
-							mv $HOME/Godot/2000px-Godot_icon.svg.png $HOME/Godot/icon.png
-
-						else
-
-							BIT="32"
-
-							echo "Downloading 32bit Version of Godot"
-							wget -P $HOME/Godot/3.0.6 https://downloads.tuxfamily.org/godotengine/3.0.6/Godot_v3.0.6-stable_x11.32.zip
-
-							echo "Downloading Icon"
-							wget -P $HOME/Godot/ https://upload.wikimedia.org/wikipedia/commons/thumb/6/6a/Godot_icon.svg/2000px-Godot_icon.svg.png
-							mv $HOME/Godot/2000px-Godot_icon.svg.png $HOME/Godot/icon.png
-
-						fi
-
-						echo "Extracting Package"
-						unzip $HOME/Godot/3.0.6/*.zip -d $HOME/Godot/3.0.6/
-
-						echo "Deleting Downloaded Package"
-						rm $HOME/Godot/3.0.6/*.zip
-
-						echo "Generating Desktp Entry"
-						DESKTOP_ENTRY=`echo "${DESKTOP_ENTRY//VERSION/$VERSION}"`
-						echo "${DESKTOP_ENTRY//BIT/$BIT}" >> Godot_v$VERSION-stable_x11.$BIT.desktop
-						#.local/share/applications/Godot_
-
-
-					else
-						if [ ! -f "$HOME/Godot/3.0.6/Godot_v3.0.6-stable_x11.64.zip" ] || [ ! -f"$HOME/Godot/3.0.6/Godot_v3.0.6-stable_x11.64.zip" ]; then
-							echo "Godot 3.0.6 is already installed in your system"
-						else
-							echo "Something Went Wrong"
-
-						fi
-					fi
-				fi
-			fi
-
-			if [[ $REPLY == 2 ]]; then
-				echo "Downloading and Installing Godot 2.1.5"
-			fi
-
-			if [[ $REPLY == 3 ]]; then
-				echo "Downloading and Installing Godot 3.1 Beta"
-			fi
-
-			if [[ $REPLY == 4 ]]; then
-				echo "Downloading and Installing Godot Export Templates"
-			fi
-		else
-			echo "Invalid Entry" >&2
+		if [[ $REPLY == 4 ]]; then
+			echo "Still Needs some work"
 		fi
 	else
-		echo "Invalid entry, Retry" >&2
+		echo "Invalid Entry" >&4
 	fi
-	exit
-fi
-if [[ $REPLY == 2 ]]; then
-	echo "Uninstall Godot Commands"
-	exit
-fi
+}
 
-else
-	echo "Invalid entry." >&2
-	exit
-fi
+#For CHecking System Architecture
+function checkbit(){
+	if [ ${MACHINE_TYPE} == 'x86_64' ]; then
+		BIT="64"
+	else
+		BIT="32"
+	fi
+}
+
+function cleanup(){
+	rm $INSTALL_DIR*.zip
+}
+
+# Running Script Calling Main Function
+main
