591f8ed6614e40abe491aeeb51da77a589801379
sarooptech <sarooptech@gmail.com>
2019-02-06 20:34:50 +0530

    removing uninstall option
    Removing Uninstall option to make it simpler. And also patching some
    minor bugs.

--- godotinstaller.sh
+++ godotinstaller.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 # This is Simple Godot Engine Installer Script
 
-# Variable Declarations
+# Variables
 
-INSTALL_DIR="$HOME/Godot/" # To ceate version directory and install perticular version
+INSTALL_DIR="$HOME/Godot" # To ceate version directory and install perticular version
 
-VERSION="" # For sttoring selected version
+VERSION="" # For storing selected version
 
 MACHINE_TYPE=`uname -m`
 
@@ -16,44 +16,24 @@ DESKTOP_ENTRY_PATH=".local/share/applications"
 DESKTOP_ENTRY="[Desktop Entry]
 Type=Application
 Name=Godot VERSION
-Icon=$HOME/Godot/icon.png
-Exec="$HOME/Godot/Godot_vVERSION-stable_x11.BIT"
+Icon=$INSTALL_DIR/icon.png
+Exec="$INSTALL_DIR/Godot_vVERSION-stable_x11.BIT"
 Comment=Game Engine
 Categories=Development;IDE;
 Terminal=false
 StartupWMClass=Godot"
 
 ICON="$HOME/Godot/icon.png"
-#-----------------------------------------
-#Function Declerations
-function main(){
 
-	checkbit
 
-	echo -e "Welcome to Godot Installer
-	(1) To Install Godot
-	(2) To Uninstall Godot
-	(0) To Exit"
+#Function Decleration
 
-	read -p "Enter selection [0,1,2] > "
-
-	if [[ $REPLY =~ ^[0-2]$ ]]; then
-		
-		if [[ $REPLY == 0 ]]; then
-			echo "Exiting Godot Installer"
-			exit
-		fi
-
-		if [[ $REPLY == 1 ]]; then
-			selectversion
-		fi
-
-		if [[ $REPLY == 2 ]]; then
-			echo "Here Goes Uninstall Code"
-		fi
-
-	# else
-	# 	echo "Invalid Entry" >&2
+#For CHecking System Architecture
+function checkbit(){
+	if [ ${MACHINE_TYPE} == 'x86_64' ]; then
+		BIT="64"
+	else
+		BIT="32"
 	fi
 }
 
@@ -71,11 +51,19 @@ function downloadbeta(){
 	wget -P $INSTALL_DIR "https://downloads.tuxfamily.org/godotengine/3.1/beta3/Godot_v3.1-beta3_x11.$BIT.zip"
 
 	downloadicon
-
+	
 	echo "Generating Desktp Entry"
-	NEW_ENTRY=`echo "${DESKTOP_ENTRY//VERSION/$VERSION}"`
-	NEW_ENTRY=`echo "${NEW_ENTRY//stable/beta3}"`
-	echo "${NEW_ENTRY//BIT/$BIT}" >> "$HOME/Desktop/Godot_v$VERSION-beta3_x11.$BIT.desktop"
+	echo "Checking if Downoad Directory Exist"
+	if [ -d "$DESKTOP_ENTRY_PATH" ]; then
+		
+		NEW_ENTRY=`echo "${DESKTOP_ENTRY//VERSION/$VERSION}"`
+		NEW_ENTRY=`echo "${NEW_ENTRY//stable/beta3}"`
+		echo "${NEW_ENTRY//BIT/$BIT}" >> "$DESKTOP_ENTRY_PATH/Godot_v$VERSION-beta3_x11.$BIT.desktop"
+	else
+		echo "Local Applications Folder Not Found"
+		echo "Created DesktopEntry at $HOME/Desktop"
+		echo "${NEW_ENTRY//BIT/$BIT}" >> "$HOME/Desktop/Godot_v$VERSION-beta3_x11.$BIT.desktop"
+	fi
 }
 
 #Function for downloading ICON
@@ -87,7 +75,7 @@ function downloadicon(){
 		wget -P $INSTALL_DIR "https://upload.wikimedia.org/wikipedia/commons/thumb/6/6a/Godot_icon.svg/2000px-Godot_icon.svg.png"
 		cd "$INSTALL_DIR"
 		mv "2000px-Godot_icon.svg.png" "icon.png"
-		cd		
+		cd
 	fi
 }
 
@@ -96,7 +84,7 @@ function checkdir(){
 	echo "Checking if Downoad Directory Exist"
 	if [ ! -d "$INSTALL_DIR" ]; then
 		echo "Directory Does Not Exist. Creating One"
-		mkdir $HOME/Godot
+		mkdir $INSTALL_DIR
 	else
 		echo "Directory Already Exist"
 	fi
@@ -104,45 +92,61 @@ function checkdir(){
 
 #For Making Desktop Entry
 function makedesktopentry(){
+	checkbit
 	echo "Generating Desktp Entry"
 	NEW_ENTRY=`echo "${DESKTOP_ENTRY//VERSION/$VERSION}"`
 
 	echo "Checking if Downoad Directory Exist"
 	if [ -d "$DESKTOP_ENTRY_PATH" ]; then
 		echo "Making Desktop Entry Under Development Section of Your Application Menu"
-		echo "${NEW_ENTRY//BIT/$BIT}" >> "$DESKTOP_ENTRY_PATH/Godot_v$VERSION-stable_x11.$BIT.desktop"		
+		echo "${NEW_ENTRY//BIT/$BIT}" >> "$DESKTOP_ENTRY_PATH/Godot_v$VERSION-stable_x11.$BIT.desktop"
 	else
 		echo "Local Applications Folder Not Found"
-		echo "Making Desktop Entry on Your Desktop"
+		echo "Making Desktop Entry at $HOME/Desktop/"
 		echo "${NEW_ENTRY//BIT/$BIT}" >> "$HOME/Desktop/Godot_v$VERSION-stable_x11.$BIT.desktop"
 	fi
 }
 
 #For Decompressing Downloaded Zip
 function decompress(){
-	unzip $HOME/Godot/*.zip -d $HOME/Godot/
+	unzip $INSTALL_DIR/*.zip -d $INSTALL_DIR
 }
 
 #For deletind downloaded temp files
 function delete(){
 	echo "Deletind Zip Files From Installation Directory"
-	rm $HOME/Godot/*.zip
+	rm $INSTALL_DIR/*.zip
+}
+
+function downloadcommands(){
+	echo "Downloading $BIT bit Version of Godot $VERSION"
+			download
+			decompress
+			downloadicon
+			makedesktopentry
+			cleanup
+}
+
+#For Cleaning Installation Files
+function cleanup(){
+	rm $INSTALL_DIR/*.zip
 }
 
 #Function For selecting Installation Version
-function selectversion(){
+function main(){
+	checkbit
 	echo "Select Godot Version To Install
 	(1) Godot 3.0.6 - Stable (Prefered)
 	(2) Godot 2.1.5 - Old Stable
 	(3) Godot 3.1 - Latest Beta Release
 	(4) Install All 3 Versions
-	(0) Back to Main Menu"
+	(0) Exit"
 	read -p "Enter Selection[1,2,3,4,0] >"
 
 	if [[ $REPLY =~ ^[0-4]$ ]]; then
 
 		if [[ $REPLY == 0 ]]; then
-			main
+			exit
 		fi
 
 		if [[ $REPLY == 1 ]]; then
@@ -183,29 +187,6 @@ function selectversion(){
 	else
 		echo "Invalid Entry"
 	fi
-}
-
-function downloadcommands(){
-	echo "Downloading $BIT bit Version of Godot $VERSION"
-			download
-			decompress
-			downloadicon
-			makedesktopentry
-			cleanup
-}
-
-#For CHecking System Architecture
-function checkbit(){
-	if [ ${MACHINE_TYPE} == 'x86_64' ]; then
-		BIT="64"
-	else
-		BIT="32"
-	fi
-}
-
-#For Cleaning Installation Files
-function cleanup(){
-	rm $INSTALL_DIR*.zip
 }
 
 # Running Script Calling Main Function
